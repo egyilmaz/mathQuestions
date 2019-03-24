454d3068a971b9d50f28db1af74784b4feac3d57
Erdem Yilmaz <erdem@artwintech>
2019-03-24 08:15:49 +0000

    Basic CSS setup, trying to align image with list item

--- questions/src/question/Question22.py
+++ questions/src/question/Question22.py
@@ -4,6 +4,7 @@ from .Types import Types
 from ..resources.Resource import boys, items, simple_fractions
 from ..utils.Utility import ask_interactive_1arg, get_two_distinct
 import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (5,1)
 from io import BytesIO
 import base64
 
@@ -22,8 +23,7 @@ class Question22(BaseQuestion):
     def graphic(self):
         plt.axis('off')
         a = r'What is  ${0} \dfrac{{{1}}}{{{2}}} + {3}\dfrac{{{4}}}{{{5}}}$ = ?'.format(self.int1,self.ratio1.numerator,self.ratio1.denominator,self.int2,self.ratio2.numerator,self.ratio2.denominator)
-        plt.text(-1, 1, a, fontsize=16)
-#        plt.show()
+        plt.text(-1, 1, a, fontsize=12)
         buf = BytesIO()
         plt.savefig(buf, format='PNG', bbox_inches='tight', pad_inches=0)
         graphic = base64.b64encode(buf.getvalue()).decode('utf-8').replace('\n','')
