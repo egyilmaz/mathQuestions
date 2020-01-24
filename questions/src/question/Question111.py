23b1e87510ceceb89007bc7028b948286fb4e02c
egyilmaz <erdemgokhanyilmaz@gmail.com>
2020-01-24 19:19:02 +0000

    Fixed question 111

--- questions/src/question/Question111.py
+++ questions/src/question/Question111.py
@@ -12,8 +12,8 @@ class Question111(BaseQuestion):
         bean_price = random.choice([240, 380, 290, 190, 210])
         tomato_price = random.choice([120,250,340,470])
         self.body = "For a can of baked bean, we need a can, 100grs of beans, 50grs of tomato. If the tomatoes " \
-                    "cost {1}pence per 500gr, beans cost {2}pence per kg and cans cost {3}pence for a bundle of 10 can, what will be the " \
-                    "cost of {3} cans of baked beans in pounds and pence"\
+                    "cost {0}pence per 500gr, beans cost {1}pence per kg and cans cost {2}pence for a bundle of 10 can" \
+                    ", what will be the cost of {3} cans of baked beans in pounds and pence"\
             .format(tomato_price, bean_price, can_price, can_amount)
         result = can_amount*(tomato_price/10 + bean_price/10 + can_price/10)
         self.res="{0}pound {1}pence".format(result//100, result%100)
